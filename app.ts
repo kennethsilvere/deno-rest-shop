0200c8d26b1025ad2bb857d5f70bce2378119690
Kenneth Silvere <kennethsilvere93@gmail.com>
2020-05-10 12:52:31 +0530

    Handle errors and improve project setup

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/denotrain/mod.ts";
+import { Application, Context } from "https://deno.land/x/denotrain/mod.ts";
 import { productsRouter } from "./api/routes/products.ts";
 import { ordersRouter } from "./api/routes/orders.ts";
 
@@ -6,5 +6,19 @@ const app = new Application({ port: 3001 });
 
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
+
+app.use((ctx: Context) => {
+    const error = new Error('Not found');
+    ctx.data = error;
+})
+
+app.use((ctx: Context) => {
+    const err = ctx.data;
+    return {
+        error: {
+            message: err.message
+        }
+    }
+});
 
 await app.run();
